0feb98937416e909d32ddb0e6a4a6ab86221749e
Eduardo <ceduardolima2000@gmail.com>
2023-10-02 17:03:44 -0300

    fucionalidade: Adicionando a dao e a entidade no banco de dados e subindo a versão para a 2

--- lib/domain/database/politech_db.dart
+++ lib/domain/database/politech_db.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
 
 import 'package:floor/floor.dart';
+import 'package:politech/domain/aluno/aluno.dart';
+import 'package:politech/domain/database/dao/aluno_dao.dart';
 import 'package:politech/domain/database/dao/usuario_dao.dart';
 import 'package:politech/domain/usuario/usuario.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 
 part 'politech_db.g.dart';
 
-@Database(version: 1, entities: [Usuario])
+@Database(version: 2, entities: [Usuario, Aluno])
 abstract class PolitechDb extends FloorDatabase {
   UsuarioDao get usuarioDao;
+  AlunoDao get alunoDao;
 }
--- lib/domain/database/politech_db.g.dart
+++ lib/domain/database/politech_db.g.dart
@@ -63,13 +63,15 @@ class _$PolitechDb extends PolitechDb {
 
   UsuarioDao? _usuarioDaoInstance;
 
+  AlunoDao? _alunoDaoInstance;
+
   Future<sqflite.Database> open(
     String path,
     List<Migration> migrations, [
     Callback? callback,
   ]) async {
     final databaseOptions = sqflite.OpenDatabaseOptions(
-      version: 1,
+      version: 2,
       onConfigure: (database) async {
         await database.execute('PRAGMA foreign_keys = ON');
         await callback?.onConfigure?.call(database);
@@ -86,6 +88,12 @@ class _$PolitechDb extends PolitechDb {
       onCreate: (database, version) async {
         await database.execute(
             'CREATE TABLE IF NOT EXISTS `usuario` (`id` TEXT NOT NULL, `cpf` TEXT NOT NULL, `nome` TEXT NOT NULL, `email` TEXT NOT NULL, PRIMARY KEY (`id`))');
+        await database.execute(
+            'CREATE TABLE IF NOT EXISTS `alunos` (`id` TEXT NOT NULL, `num_inscricao` TEXT NOT NULL, `nome` TEXT NOT NULL, `cpf` TEXT NOT NULL, PRIMARY KEY (`id`))');
+        await database.execute(
+            'CREATE UNIQUE INDEX `index_alunos_num_inscricao` ON `alunos` (`num_inscricao`)');
+        await database.execute(
+            'CREATE UNIQUE INDEX `index_alunos_cpf` ON `alunos` (`cpf`)');
 
         await callback?.onCreate?.call(database, version);
       },
@@ -96,6 +104,11 @@ class _$PolitechDb extends PolitechDb {
   @override
   UsuarioDao get usuarioDao {
     return _usuarioDaoInstance ??= _$UsuarioDao(database, changeListener);
+  }
+
+  @override
+  AlunoDao get alunoDao {
+    return _alunoDaoInstance ??= _$AlunoDao(database, changeListener);
   }
 }
 
@@ -186,5 +199,112 @@ class _$UsuarioDao extends UsuarioDao {
   @override
   Future<void> atualizar(Usuario usuario) async {
     await _usuarioUpdateAdapter.update(usuario, OnConflictStrategy.abort);
+  }
+}
+
+class _$AlunoDao extends AlunoDao {
+  _$AlunoDao(
+    this.database,
+    this.changeListener,
+  )   : _queryAdapter = QueryAdapter(database, changeListener),
+        _alunoInsertionAdapter = InsertionAdapter(
+            database,
+            'alunos',
+            (Aluno item) => <String, Object?>{
+                  'id': item.id,
+                  'num_inscricao': item.numInscricao,
+                  'nome': item.nome,
+                  'cpf': item.cpf
+                },
+            changeListener),
+        _alunoUpdateAdapter = UpdateAdapter(
+            database,
+            'alunos',
+            ['id'],
+            (Aluno item) => <String, Object?>{
+                  'id': item.id,
+                  'num_inscricao': item.numInscricao,
+                  'nome': item.nome,
+                  'cpf': item.cpf
+                },
+            changeListener);
+
+  final sqflite.DatabaseExecutor database;
+
+  final StreamController<String> changeListener;
+
+  final QueryAdapter _queryAdapter;
+
+  final InsertionAdapter<Aluno> _alunoInsertionAdapter;
+
+  final UpdateAdapter<Aluno> _alunoUpdateAdapter;
+
+  @override
+  Stream<List<Aluno>> listar() {
+    return _queryAdapter.queryListStream('SELECT * From alunos LIMIT 20',
+        mapper: (Map<String, Object?> row) => Aluno(
+            row['id'] as String,
+            row['num_inscricao'] as String,
+            row['nome'] as String,
+            row['cpf'] as String),
+        queryableName: 'alunos',
+        isView: false);
+  }
+
+  @override
+  Stream<List<Aluno>> listarComLimite(int limite) {
+    return _queryAdapter.queryListStream('SELECT * From alunos LIMIT ?1',
+        mapper: (Map<String, Object?> row) => Aluno(
+            row['id'] as String,
+            row['num_inscricao'] as String,
+            row['nome'] as String,
+            row['cpf'] as String),
+        arguments: [limite],
+        queryableName: 'alunos',
+        isView: false);
+  }
+
+  @override
+  Future<Aluno?> procurarPorId(String id) async {
+    return _queryAdapter.query('SELECT * From alunos WHERE id=?1',
+        mapper: (Map<String, Object?> row) => Aluno(
+            row['id'] as String,
+            row['num_inscricao'] as String,
+            row['nome'] as String,
+            row['cpf'] as String),
+        arguments: [id]);
+  }
+
+  @override
+  Future<List<Aluno>> procurarPorNome(String nome) async {
+    return _queryAdapter.queryList(
+        'SELECT * FROM alunos WHERE nome LIKE ?1 LIMIT 20',
+        mapper: (Map<String, Object?> row) => Aluno(
+            row['id'] as String,
+            row['num_inscricao'] as String,
+            row['nome'] as String,
+            row['cpf'] as String),
+        arguments: [nome]);
+  }
+
+  @override
+  Future<Aluno?> procurarPorInscricao(String inscricao) async {
+    return _queryAdapter.query('SELECT * From alunos WHERE num_inscricao=?1',
+        mapper: (Map<String, Object?> row) => Aluno(
+            row['id'] as String,
+            row['num_inscricao'] as String,
+            row['nome'] as String,
+            row['cpf'] as String),
+        arguments: [inscricao]);
+  }
+
+  @override
+  Future<void> inserir(Aluno aluno) async {
+    await _alunoInsertionAdapter.insert(aluno, OnConflictStrategy.abort);
+  }
+
+  @override
+  Future<void> atualizar(Aluno usuario) async {
+    await _alunoUpdateAdapter.update(usuario, OnConflictStrategy.abort);
   }
 }

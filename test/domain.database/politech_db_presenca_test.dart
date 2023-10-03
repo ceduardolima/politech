ff97ca2aaaeead008be1c559be1e172956a2c0bd
Eduardo <ceduardolima2000@gmail.com>
2023-10-03 14:28:58 -0300

    test: criando testes para o caso de excluir e inserir uma lista

--- test/domain.database/politech_db_presenca_test.dart
+++ test/domain.database/politech_db_presenca_test.dart
@@ -7,7 +7,7 @@ import 'package:politech/domain/database/politech_db.dart';
 import 'package:politech/domain/presenca/presenca.dart';
 import 'package:politech/domain/turma/turma.dart';
 
-void main()  {
+void main() {
   group("Teste da entidade Presenca", () {
     late PolitechDb banco;
     late PresencaDao presencaDao;
@@ -15,9 +15,7 @@ void main()  {
     late TurmaDao turmaDao;
 
     setUp(() async {
-      banco = await $FloorPolitechDb
-          .inMemoryDatabaseBuilder()
-          .build();
+      banco = await $FloorPolitechDb.inMemoryDatabaseBuilder().build();
       alunoDao = banco.alunoDao;
       presencaDao = banco.presencaDao;
       turmaDao = banco.turmaDao;
@@ -33,16 +31,18 @@ void main()  {
       final aluno_1 = Aluno.genId("12345", "Eduardo", "123", turma_1.id);
       final aluno_2 = Aluno.genId("123456", "Eduardo", "1234", turma_1.id);
       turmaDao.inserir(turma_1);
-      alunoDao.inserir(aluno_1);
-      alunoDao.inserir(aluno_2);
-      presencaDao.inserir(Presenca.genId(aluno_1.id, true, now, turma_1.id));
-      presencaDao.inserir(Presenca.genId(aluno_2.id, true, now, turma_1.id));
+      alunoDao.inserirLista([aluno_1, aluno_2]);
+      presencaDao.inserirLista([
+        Presenca.genId(aluno_1.id, true, now, turma_1.id),
+        Presenca.genId(aluno_2.id, true, now, turma_1.id)
+      ]);
       final lista = await presencaDao.listarAlunosPresentes();
       expect(lista, const TypeMatcher<List<Aluno>>());
       expect(lista.length, equals(2));
     });
 
-    test("Listando presenca de um aluno em um determinado período de tempo", () async {
+    test("Listando presenca de um aluno em um determinado período de tempo",
+        () async {
       final now = DateTime.now();
       final day_1 = DateTime(2023, 9, 29);
       final day_2 = DateTime(2023, 9, 30);
@@ -50,12 +50,37 @@ void main()  {
       final aluno_1 = Aluno.genId("12345", "Eduardo", "123", turma_1.id);
       turmaDao.inserir(turma_1);
       alunoDao.inserir(aluno_1);
-      presencaDao.inserir(Presenca.genId(aluno_1.id, true, now, turma_1.id));
-      presencaDao.inserir(Presenca.genId(aluno_1.id, true, day_1, turma_1.id));
-      presencaDao.inserir(Presenca.genId(aluno_1.id, true, day_2, turma_1.id));
-      final lista = await presencaDao.listarPresencasDoAluno(aluno_1.id, day_2, now, true);
+      presencaDao.inserirLista([
+        Presenca.genId(aluno_1.id, true, now, turma_1.id),
+        Presenca.genId(aluno_1.id, true, day_1, turma_1.id),
+        Presenca.genId(aluno_1.id, true, day_2, turma_1.id)
+      ]);
+      final lista = await presencaDao.listarPresencasDoAluno(
+          aluno_1.id, day_2, now, true);
       expect(lista, const TypeMatcher<List<Presenca>>());
       expect(lista.length, equals(2));
     });
+
+    test("Exlcuindo presenca",
+            () async {
+          final now = DateTime.now();
+          final day_1 = DateTime(2023, 9, 29);
+          final day_2 = DateTime(2023, 9, 30);
+          final turma_1 = Turma.genId("Turma 01");
+          final aluno_1 = Aluno.genId("12345", "Eduardo", "123", turma_1.id);
+          final presenca =  Presenca.genId(aluno_1.id, true, now, turma_1.id);
+          turmaDao.inserir(turma_1);
+          alunoDao.inserir(aluno_1);
+          presencaDao.inserirLista([
+            presenca,
+            Presenca.genId(aluno_1.id, true, day_1, turma_1.id),
+            Presenca.genId(aluno_1.id, true, day_2, turma_1.id)
+          ]);
+          presencaDao.excluir(presenca);
+          final listaPresenca = await presencaDao.listarPresencasDoAluno(aluno_1.id, day_1, now, true);
+          expect(listaPresenca, const TypeMatcher<List<Presenca>>());
+          expect(listaPresenca.length, equals(2));
+        });
+
   });
 }

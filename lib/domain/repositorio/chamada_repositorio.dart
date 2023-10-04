84e3d7de181501f1e5c6254564f2ca242d27a446
Eduardo <ceduardolima2000@gmail.com>
2023-10-04 11:08:42 -0300

    funcionalidade: Criando repositório e viewModel da chamada dao

---
+++ lib/domain/repositorio/chamada_repositorio.dart
@@ -0,0 +1,36 @@
+import 'package:get_it/get_it.dart';
+import 'package:politech/domain/database/dao/chamada_dao.dart';
+
+import '../chamada/chamada.dart';
+
+class ChamadaRepositorio {
+  late ChamadaDao _chamadaDao;
+
+  ChamadaRepositorio() {
+    _chamadaDao = GetIt.instance.get<ChamadaDao>();
+  }
+
+  Future<void> inserir(Chamada chamada) async {
+    await _chamadaDao.inserir(chamada);
+  }
+
+  Future<void> inserirLista(List<Chamada> chamada) async {
+    await _chamadaDao.inserirLista(chamada);
+}
+
+  Stream<List<Chamada>> assistirChamadasDaTurma(String turmaId) async* {
+    yield* _chamadaDao.assistirChamadasDaTurma(turmaId);
+  }
+
+  Future<List<Chamada>> listarChamadasDaTurma(String turmaId) async {
+    return await _chamadaDao.listarChamadasDaTurma(turmaId);
+  }
+
+  Future<void> atualizar(Chamada chamada) async {
+    await _chamadaDao.atualizar(chamada);
+  }
+
+  Future<void> excluir(Chamada chamada) async {
+    await _chamadaDao.excluir(chamada);
+  }
+}

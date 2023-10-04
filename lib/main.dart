f0a0ba2d2b82541b2a99c450aa2d44e5629fac09
Eduardo <ceduardolima2000@gmail.com>
2023-10-04 15:34:07 -0300

    funcionalidade: definindo providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:politech/domain/database/dao/aluno_dao.dart';
+import 'package:politech/domain/database/dao/chamada_dao.dart';
 import 'package:politech/domain/database/dao/presenca_dao.dart';
 import 'package:politech/domain/database/dao/turma_dao.dart';
 import 'package:politech/domain/database/dao/usuario_dao.dart';
 import 'package:politech/domain/database/politech_db.dart';
+import 'package:politech/domain/repositorio/aluno_repositorio.dart';
+import 'package:politech/domain/repositorio/chamada_repositorio.dart';
+import 'package:politech/domain/repositorio/presenca_repositorio.dart';
+import 'package:politech/domain/repositorio/turma_repositorio.dart';
+import 'package:politech/domain/repositorio/usuario_repositorio.dart';
+import 'package:politech/paginas/chamada_pagina.dart';
 import 'package:politech/theme/colors_theme.dart';
+import 'package:politech/viewModels/aluno_view_model.dart';
+import 'package:politech/viewModels/chamada_view_model.dart';
+import 'package:politech/viewModels/presenca_view_model.dart';
+import 'package:politech/viewModels/turma_view_model.dart';
+import 'package:politech/viewModels/usuario_view_model.dart';
+import 'package:provider/provider.dart';
+
+import 'domain/turma/turma.dart';
 
 void main() {
   criarSingletons();
   runApp(
-    MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorsTheme().lightColorsScheme()
-      ),
-      home: FutureBuilder(
-        future: GetIt.instance.allReady(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            return MyHomePage(title: "Politech");
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChamadaViewModel()),
+        ChangeNotifierProvider(create: (_) => TurmaViewModel()),
+        ChangeNotifierProvider(create: (_) => AlunoViewModel()),
+        ChangeNotifierProvider(create: (_) => PresencaViewModel()),
+        ChangeNotifierProvider(create: (_) => UsuarioViewModel())
+      ],
+      child: MaterialApp(
+        theme: ThemeData(colorScheme: ColorsTheme().lightColorsScheme()),
+        home: FutureBuilder(
+          future: GetIt.instance.allReady(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ChamadaPagina(turma: Turma("1", "MF - TURMA"));
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
       ),
     ),
   );
@@ -46,124 +68,22 @@ void criarSingletons() {
   getIt.registerSingletonWithDependencies<PresencaDao>(
       () => GetIt.instance.get<PolitechDb>().presencaDao,
       dependsOn: [PolitechDb]);
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  getIt.registerSingletonWithDependencies<ChamadaDao>(
+      () => GetIt.instance.get<PolitechDb>().chamadaDao,
+      dependsOn: [PolitechDb]);
+  getIt.registerSingletonWithDependencies<UsuarioRepositorio>(
+      () => UsuarioRepositorio(),
+      dependsOn: [PolitechDb, UsuarioDao]);
+  getIt.registerSingletonWithDependencies<AlunoRepositorio>(
+      () => AlunoRepositorio(),
+      dependsOn: [PolitechDb, AlunoDao]);
+  getIt.registerSingletonWithDependencies<TurmaRepositorio>(
+      () => TurmaRepositorio(),
+      dependsOn: [PolitechDb, TurmaDao]);
+  getIt.registerSingletonWithDependencies<PresencaRepositorio>(
+      () => PresencaRepositorio(),
+      dependsOn: [PolitechDb, PresencaDao]);
+  getIt.registerSingletonWithDependencies<ChamadaRepositorio>(
+      () => ChamadaRepositorio(),
+      dependsOn: [PolitechDb, ChamadaDao]);
 }

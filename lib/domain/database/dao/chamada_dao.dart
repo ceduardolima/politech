dd7926e3de02e0f575a3e89ae35002a0f49c8a04
Eduardo <ceduardolima2000@gmail.com>
2023-10-04 10:29:43 -0300

    funcionalidade: Criando dao para a entidade Chamada

---
+++ lib/domain/database/dao/chamada_dao.dart
@@ -0,0 +1,23 @@
+import 'package:floor/floor.dart';
+import 'package:politech/domain/chamada/chamada.dart';
+
+@dao
+abstract class ChamadaDao {
+  @Insert(onConflict: OnConflictStrategy.fail)
+  Future<void> inserir(Chamada chamada);
+
+  @Insert(onConflict: OnConflictStrategy.fail)
+  Future<void> inserirLista(List<Chamada> chamada);
+
+  @Query("SELECT * FROM chamadas WHERE turma_id = :turmaId")
+  Stream<List<Chamada>> assistirChamadasDaTurma(String turmaId);
+
+  @Query("SELECT * FROM chamadas WHERE turma_id = :turmaId")
+  Future<List<Chamada>> listarChamadasDaTurma(String turmaId);
+
+  @update
+  Future<void> atualizar(Chamada chamada);
+
+  @delete
+  Future<void> excluir(Chamada chamada);
+}

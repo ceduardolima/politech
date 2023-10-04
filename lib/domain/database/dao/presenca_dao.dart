187ac8702480bd08cb2b848a312da86ac7a78d33
Eduardo <ceduardolima2000@gmail.com>
2023-10-04 11:07:12 -0300

    refatorando: Atualizando dependência da entidade presenca

--- lib/domain/database/dao/presenca_dao.dart
+++ lib/domain/database/dao/presenca_dao.dart
@@ -10,8 +10,8 @@ abstract class PresencaDao {
   @Insert(onConflict: OnConflictStrategy.fail)
   Future<void> inserirLista(List<Presenca> presenca);
 
-  @Query("SELECT presencas.* FROM turmas LEFT JOIN presencas ON turmas.id= presencas.turma_id WHERE presencas.turma_id = :turmaId")
-  Stream<List<Presenca>> assistirListaDePresencaDaTurma(String turmaId);
+  @Query("SELECT * FROM presencas WHERE chamada_id = :chamadaId")
+  Stream<List<Presenca>> assistirListaDePresencaDaTurma(String chamadaId);
 
   @Query("SELECT alunos.* FROM alunos LEFT JOIN presencas ON alunos.id = presencas.aluno_id WHERE presente = 1")
   Future<List<Aluno>> listarAlunosPresentes();

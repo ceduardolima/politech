dfa3b4add1d04bc8fbfcb44cc453d847a777196e
Eduardo <ceduardolima2000@gmail.com>
2023-10-05 15:04:24 -0300

    funcionalidade: Criando dialog com apenas um botao de confirmar

---
+++ lib/widgets/dialogs/dialog_botao_unico.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class DialogBotaoUnico extends StatelessWidget {
+  const DialogBotaoUnico({
+    super.key,
+    required this.titulo,
+    this.label,
+    required this.confirmar,
+    required this.fecharDialog,
+    required this.filho,
+  });
+
+  final Widget filho;
+  final String? label;
+  final String titulo;
+  final Function() confirmar;
+  final Function() fecharDialog;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const Spacer(flex: 6),
+              Text(
+                titulo,
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const Spacer(flex: 3),
+              IconButton(
+                onPressed: fecharDialog,
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                ),
+              )
+            ],
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 40,
+            ),
+            child: filho,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size.fromHeight(60),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero,
+              ),
+            ),
+            onPressed: confirmar,
+            child: Text(label ?? "CONFIRMAR"),
+          )
+        ],
+      ),
+    );
+  }
+}

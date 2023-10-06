a3ad884e93073c07a5a71109c3e371798fb554a9
Eduardo <ceduardolima2000@gmail.com>
2023-10-06 09:41:19 -0300

    refatorando: mudando o formato do botão de cadastro

--- lib/paginas/telalogin.dart
+++ lib/paginas/telalogin.dart
@@ -10,118 +10,84 @@ class TelaLogin extends StatelessWidget {
     return Scaffold(
       backgroundColor: ColorsTheme().lightColorsScheme().primary,
       body: Center(
-        child: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8.0),
+            boxShadow: [
+              BoxShadow(
+                color: ColorsTheme()
+                    .lightColorsScheme()
+                    .primaryContainer
+                    .withOpacity(0.5),
+                spreadRadius: 0,
+                blurRadius: 7,
+                offset: Offset(5, 6),
+              ),
+            ],
+          ),
+          width: 300.0,
+          padding: const EdgeInsets.all(30.0),
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                height: 120,
-                width: 120,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 0,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
+              const Text(
+                'PoliTech',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, // Texto em negrito
+                  fontSize: 24,
                 ),
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.transparent,
-                  child: Icon(
-                    Icons.person,
-                    size: 50,
-                    color: ColorsTheme().lightColorsScheme().primary,
-                  ),
+                textAlign: TextAlign.center,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nome de Usuário',
                 ),
               ),
-              SizedBox(height: 40.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: ColorsTheme()
-                          .lightColorsScheme()
-                          .primaryContainer
-                          .withOpacity(0.5),
-                      spreadRadius: 0,
-                      blurRadius: 7,
-                      offset: Offset(5, 6),
-                    ),
-                  ],
+              const TextField(
+                obscureText: true, // Para ocultar a senha enquanto é digitada
+                decoration: InputDecoration(
+                  labelText: 'Senha',
                 ),
-                width: 300.0,
-                height: 305.0,
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text(
-                      'PoliTech',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold, // Texto em negrito
-                        fontSize: 24,
-                      ),
+              ),
+              SizedBox(height: 30.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(fixedSize: Size.fromHeight(40)),
+                onPressed: () {
+                  // Mostrar o texto "Testar banco" ao pressionar o botão
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Testar banco'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Fechar'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Login', style: TextStyle(fontSize: 16),),
+              ),
+              const SizedBox(height: 10,),
+              TextButton(
+                onPressed: () {
+                  // Navegar para a TelaCadastro quando o botão "Cadastrar" for pressionado
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TelaCadastro(),
                     ),
-                    const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Nome de Usuário',
-                      ),
-                    ),
-                    const TextField(
-                      obscureText: true, // Para ocultar a senha enquanto é digitada
-                      decoration: InputDecoration(
-                        labelText: 'Senha',
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        ElevatedButton(
-                          onPressed: () {
-                            // Mostrar o texto "Testar banco" ao pressionar o botão
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: const Text('Testar banco'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: const Text('Fechar'),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                          child: Text('Login'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            // Navegar para a TelaCadastro quando o botão "Cadastrar" for pressionado
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => TelaCadastro(),
-                              ),
-                            );
-                          },
-                          child: Text('Cadastrar'),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                  );
+                },
+                child: Text('Realize seu cadastro aqui!'),
               ),
             ],
           ),

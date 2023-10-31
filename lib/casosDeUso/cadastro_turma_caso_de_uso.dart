a99b6b88d66e7d033535f374b3235f54c2f44ed3
Eduardo <ceduardolima2000@gmail.com>
2023-10-31 09:15:39 -0300

    funcionalidade: criando caso de uso para o cadastro de turmas

--- lib/casosDeUso/cadastro_turma_caso_de_uso.dart
+++ lib/casosDeUso/cadastro_turma_caso_de_uso.dart
@@ -1,20 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:politech/domain/aluno/aluno.dart';
+import 'package:politech/domain/horario/horario.dart';
+
 import '../domain/turma/turma.dart';
 
 class CadastroTurmaCasoDeUso {
-  late Map _turmasMap;
+  final List<Turma> _turmas = [];
+  final List<Aluno> _alunos = [];
+  final List<Horario> _horarios = [];
 
   CadastroTurmaCasoDeUso(Map turmas) {
-    _turmasMap = turmas;
+    _criarListaDeTumas(turmas);
   }
 
-  List<Turma> obterTurmas() {
-    final chaves = _turmasMap.keys.toList();
-    final List<Turma> turmas = [];
+  _criarListaDeTumas(Map turmasMap) {
+    final chaves = turmasMap.keys.toList();
     for(var chave in chaves) {
-      Map turmaMap = _turmasMap[chave];
-      var turma = Turma(chave, turmaMap["nome"], turmaMap["codigo"]);
-      turmas.add(turma);
+      Map turmaMap = turmasMap[chave];
+      _criarAlunos(chave, turmaMap["alunos"]);
+      _criarHorarios(chave, turmaMap["horarios"]);
+      _turmas.add(Turma(chave, turmaMap["nome"], turmaMap["codigo"]));
     }
-    return turmas;
   }
+
+  _criarAlunos(String turmaId, List alunos) {
+    final List<Aluno> bufferAlunos = [];
+    for(var aluno in alunos) {
+      bufferAlunos.add(Aluno.genId(aluno, turmaId));
+    }
+    _alunos.addAll(bufferAlunos);
+  }
+
+  _criarHorarios(String turmaId, List horarios) {
+    final List<Horario> bufferHorarios = [];
+    for (var horario in horarios) {
+      bufferHorarios.add(Horario.gen(horario["dia"], horario["horario"], horario["sala"], turmaId));
+    }
+    _horarios.addAll(bufferHorarios);
+  }
+
+  List<Aluno> get alunos => _alunos;
+
+  List<Turma> get turmas => _turmas;
+
+  List<Horario> get horarios => _horarios;
 }
